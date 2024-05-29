f1b874b75035ca189de4132704b071cb3b276cbd
Gustavo Silva Vieira <gstvsvieira@gmail.com>
2024-05-29 14:21:54 -0300

    fix image path

--- src/js/intro.js
+++ src/js/intro.js
@@ -4,15 +4,19 @@ const introSub = document.querySelector(".intro-sub")
 const intro = document.querySelector(".intro")
 const introImg = document.querySelector(".intro-img")
 
-intro.addEventListener("mouseenter", () => {
-    intro.style.backgroundColor = "rgb(0, 0, 0, 0.8)"
-    intro.style.color = "white";
-    introImg.src = "../../assets/images/AssinaturaHorizontal-Branco.png"
+function getImagePath(relativePath) {
+    const basePath = window.location.pathname.includes('src/html') ? '../../' : './';
+    return basePath + relativePath;
+}
 
-})
+intro.addEventListener("mouseenter", () => {
+    intro.style.backgroundColor = "rgb(0, 0, 0, 0.8)";
+    intro.style.color = "white";
+    introImg.src = getImagePath('assets/images/AssinaturaHorizontal-Branco.png');
+});
 
 intro.addEventListener("mouseleave", () => {
-    intro.style.backgroundColor = "transparent"
-    intro.style.color = "black"
-    introImg.src = "../../assets/images/AssinaturaHorizontal.png"
-})
+    intro.style.backgroundColor = "transparent";
+    intro.style.color = "black";
+    introImg.src = getImagePath('assets/images/AssinaturaHorizontal.png');
+});
--- src/js/menu.js
+++ src/js/menu.js
@@ -13,18 +13,28 @@ menu.addEventListener("click", () => {
         meio.style.display = "none";
         cima.style.transform = "translateY(10px) rotate(45deg) ";
         baixo.style.transform = "translateY(-10px) rotate(-45deg)";
-        fhlist.style.transform = "translateX(0)";
         partes.forEach((parte,i) => {
             parte.style.backgroundColor = "var(--cor2)";
         })
+
+        fhlist.style.display = "flex";
+        setTimeout(() => {
+            fhlist.style.transform = "translateX(0)";
+        }, 200);
+        
     }else{
         meio.style.display = "block";
         cima.style.transform = "translateY(0) rotate(0)";
         baixo.style.transform = "translateY(0) rotate(0)";
-        fhlist.style.transform = "translateX(100%)";
         partes.forEach((parte,i) => {
             parte.style.backgroundColor = "var(--cor1)";
         })
+
+        fhlist.style.transform = "translateX(100%)";
+        setTimeout(() => {
+            fhlist.style.display = "none";
+        }, 1000);
+        
     }
 
     console.log(menuOpen);
